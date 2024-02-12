7597b0a11da112f1d68417334213d2e0dc057304
Nihar Patel <patelnihar14@gmail.com>
2024-02-12 14:42:55 +0530

    .

--- Projects/Project3/src/components/index.js
+++ Projects/Project3/src/components/index.js
@@ -3,5 +3,29 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import Postform from "./postform/Postform";
+import AuthLayout from "./AuthLayout";
+import Button from "./Button";
+import Input from "./Input";
+import RTE from "./RTE";
+import Select from "./Select";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Signup,
+  Login,
+  PostCard,
+  Postform,
+  AuthLayout,
+  Button,
+  Input,
+  RTE,
+  Select,
+};

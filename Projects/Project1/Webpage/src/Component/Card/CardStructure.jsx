10abeeaada725fc6b96822682a7b26c269a1baa9
Nihar Patel <patelnihar14@gmail.com>
2024-02-15 13:27:51 +0530

    .

--- Projects/Project1/Webpage/src/Component/Card/CardStructure.jsx
+++ Projects/Project1/Webpage/src/Component/Card/CardStructure.jsx
@@ -1,44 +1,48 @@
-import React from 'react';
+import React from "react";
 import "./Card.css";
-import { useNavigate } from 'react-router-dom';
-import { userAuth } from '../../Pages/Auth/Auth';
+import { useNavigate } from "react-router-dom";
+import { userAuth } from "../../Pages/Auth/Auth";
 
 function CardStructure({ data }) {
+  const Navigate = useNavigate();
 
-    const Navigate = useNavigate();
+  let { user } = userAuth();
 
-    let { user } = userAuth();
-
-    function CardNav() {
-        if (!user) {
-            Navigate('/alertbox');
-
-        } else {
-            Navigate('/plant');
-        }
-
+  function CardNav() {
+    if (!user) {
+      Navigate("/alertbox");
+    } else {
+      Navigate("/plant");
     }
-    return (
-        <>
-            <div className="Cardmain">
+  }
+  return (
+    <>
+      <div className="Cardmain">
+        <img src={data?.img} className="card_img" />
 
-                <img src={data?.img} />
+        <div className="card_det">
+          <div className="inner-det">
+            <h4>{data?.name}</h4>
+            <p className="card-desc3">{data?.description3}</p>
+            <span className="card-price">{data?.price}</span>
+            <h6 onClick={() => CardNav()} className="card_det-link">
+              {data?.link}
+            </h6>
+          </div>
+        </div>
 
-                <div className='card_det'>
-                    <div className="inner-det">
-                        <h4>{data?.name}</h4>
-                        <p className='card-desc3'>{data?.description3}</p>
-                        <span className='card-price'>{data?.price}</span>
-                        <h6 onClick={() => CardNav()} className='card_det-link'>{data?.link}</h6>
-                    </div>
-                </div>
-
-                <div className="cart-logo">
-                    <acronym title="Add to cart"> <a href=""> <i className="fa-solid fa-cart-arrow-down"></i></a></acronym>
-                </div>
-            </div>
-        </>
-    );
+        <div className="cart-logo">
+          <acronym title="Add to cart">
+            {" "}
+            <a href="">
+              {" "}
+              <i className="fa-solid fa-cart-arrow-down"></i>
+            </a>
+          </acronym>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default CardStructure;
